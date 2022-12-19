53c6cdd9a94f46c914bb1c19aeefa4a1493b3bdd
nad388 <nad388@yandex.ru>
2022-12-19 22:24:44 +0600

    added LoginForm component

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,30 @@
+import { Button, Form, Input } from 'antd';
+import React, { FC } from 'react';
+
+const LoginForm: FC = () => {
+  return (
+    <Form>
+      <Form.Item
+        label='Username'
+        name='username'
+        rules={[{ required: true, message: 'Please input your username!' }]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label='Password'
+        name='password'
+        rules={[{ required: true, message: 'Please input your password!' }]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item>
+        <Button type='primary' htmlType='submit'>
+          Login
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default LoginForm;
