b8d8dae98b494d85c18b26706676935f1b75caac
nad388 <nad388@yandex.ru>
2022-12-22 16:13:22 +0600

    added Calendar

---
+++ src/components/EventForm.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+const EventForm: FC = () => {
+  return <div></div>;
+};
+
+export default EventForm;
--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,7 +1,23 @@
-import React, { FC } from 'react';
+import { Button, Layout, Modal, Row } from 'antd';
+import React, { FC, useState } from 'react';
+import EventCalendar from '../components/EventCalendar';
 
 const Event: FC = () => {
-  return <div>EVENT</div>;
+  const [modalVisible, setModalVisible] = useState(false);
+  return (
+    <Layout>
+      <EventCalendar events={[]} />
+      <Row justify='center'>
+        <Button onClick={() => setModalVisible(true)}>Add event</Button>
+      </Row>
+      <Modal
+        title='Add event'
+        open={modalVisible}
+        footer={null}
+        onCancel={() => setModalVisible(false)}
+      ></Modal>
+    </Layout>
+  );
 };
 
 export default Event;
