c2ed8da207370a3ac522f6b08e78d2dc182f2848
nad388 <nad388@yandex.ru>
2022-12-22 21:23:34 +0600

    added events reducer

--- src/components/EventForm.tsx
+++ src/components/EventForm.tsx
@@ -1,7 +1,46 @@
+import { Button, DatePicker, Form, Input, Row, Select } from 'antd';
 import React, { FC } from 'react';
+import { rules } from '../utils/rules';
 
 const EventForm: FC = () => {
-  return <div></div>;
+  return (
+    <Form>
+      <Form.Item
+        label='Event description'
+        name='description'
+        rules={[rules.required()]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label='Event date'
+        name='description'
+        rules={[rules.required()]}
+      >
+        <DatePicker />
+      </Form.Item>
+      <Form.Item
+        label='Event name'
+        name='description'
+        rules={[rules.required()]}
+      >
+        <Select>
+          <Select.Option value='jack'>Jack</Select.Option>
+          <Select.Option value='lucy'>Lucy</Select.Option>
+          <Select.Option value='disabled' disabled>
+            disabled
+          </Select.Option>
+        </Select>
+      </Form.Item>
+      <Row justify={'end'}>
+        <Form.Item>
+          <Button type='primary' htmlType='submit'>
+            Create
+          </Button>
+        </Form.Item>
+      </Row>
+    </Form>
+  );
 };
 
 export default EventForm;
